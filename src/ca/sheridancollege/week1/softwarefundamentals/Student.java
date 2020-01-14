37a22c0305a419254ddf8342c10e2cf643800733
sandhvan <vanshsandhu84400@gmail.com>
2020-01-14 17:53:15 -0500

    Revert "Second Commit"
    This reverts commit d82c89eec8c9efb8b433fc1118910503eb5b810e.

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -3,7 +3,6 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
 
     private String name;
-    private int studentId;
 
     public String getName() {
         return name;
