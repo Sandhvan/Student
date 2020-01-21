4c2e5dce657b874d8f7f0d54e4020b66c6cc6e55
test <>
2020-01-21 16:25:20 -0500

    Thi

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -1,8 +1,37 @@
 package ca.sheridancollege.week1.softwarefundamentals;
+//to canjidwuinevidouvnowesz
+//hcuwucwuhwiwebweb
 
 public class Student {
 
     private String name;
+    private int gpa;
+    private String email;
+    private String city;
+
+    public int getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(int gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public String getName() {
         return name;
